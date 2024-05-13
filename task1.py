908707b4219ce541732bee0a82b162d0163eca68
AbhinashBisai <abhinashbisai17@gmail.com>
2024-05-13 10:44:38 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+#arithematic operations
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("reminder:",a%b)
+#Strings
+S="abhinash 7788"
+print(S.replace("abhi","anil"))
+print(S.find("i"))
+#conditionALal Statements
+i=15
+if(i<20):
+    print("i is least")
+else:
+    print("i is larger")
